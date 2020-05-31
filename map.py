04f60faaf7f7ec94d245bf8eaca32d4ab131e436
bowe99 <kevinbowe1999@gmail.com>
2020-05-30 22:36:32 -0400

    Add colors to markets determined by height of the mountain

--- map.py
+++ map.py
@@ -16,7 +16,13 @@ map = folium.Map(location=[38.5, -63.09], zoom_start=6, tiles='Stamen Terrain')
 fg = folium.FeatureGroup(name='My Map')
 
 for lat, lon, elv in zip(latitudes, longitudes, elevations):
-    fg.add_child(folium.Marker(location=[lat, lon], popup='%sm' % elv, icon=folium.Icon(color='green')))
+    if elv < 1800:
+        color = 'green'
+    elif elv >= 1800 and elv <= 2100:
+        color = 'orange'
+    else:
+        color = 'red'
+    fg.add_child(folium.Marker(location=[lat, lon], popup='%sm' % elv, icon=folium.Icon(color=color)))
 
 map.add_child(fg)
 map.save('Map1.html')
